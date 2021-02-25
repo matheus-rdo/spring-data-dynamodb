cfd7e653719820a16f86e1c9a197d230654b529a
matheushr97 <matheushr97@gmail.com>
2021-02-24 22:48:26 -0300

    Init project - spring 2.3.9 + Java 11

---
+++ src/main/java/com/github/matheushr97/eventaudit/SpringDataEventAuditApplication.java
@@ -0,0 +1,13 @@
+package com.github.matheushr97.eventaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataEventAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataEventAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/matheushr97/eventaudit/SpringDataEventAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheushr97.eventaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataEventAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
